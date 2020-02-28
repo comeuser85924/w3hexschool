e69d67291e3dcac4fff8a90dd628fa2a301f739f
kevinlai0924 <kevin@verywed.com>
2020-02-28 19:57:24 +0800

    fixed_import_code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
-<template>
-  <div id="app" class="container">
-    <img src="./assets/logo.png">
-    <div class="table-responsive">
+  <template>
+    <div id="app" class="container">
+      <!-- log圖片 -->
+      <img src="./assets/logo.png">
+      <!-- 我們的標題 -->
       <h1>w3HexSchool 鼠年全馬鐵人挑戰 - 查詢系統</h1>
       <v-client-table ref="myTable" :data="tableData" :columns="columns" :options="options">
-        <template   slot="name" slot-scope="props">
-          <a class="text-left textover"  :href="props.row.blogUrl">{{ props.row.name }}</a>
+        <template   slot="名字" slot-scope="props">
+          {{props}}
+          <a  class="text-left textover"  :href="props.row.blogUrl">{{ props.row.name }}</a>
         </template>
-        <template   slot="blogList" slot-scope="props">
+        <template   slot="部落格文章" slot-scope="props">
           <!-- <button @click="openModal(props.row.blogList)">詳細</button>    -->
           <a v-for="(item,i) in props.row.blogList" :key="i" :href="item.url">{{ item.title }}<br></a>
         </template>
+        <template   slot="更新時間" slot-scope="props">
+          {{ props.row.updateTime }}
+        </template>
       </v-client-table>
-    </div>
-  </div>
-</template>
 
-<script>
-import axios from 'axios';
-export default {
-  name: 'App',
-  data() {
-    return {
-      columns: [ "name", "blogList","updateTime"],
-      tableData: [],
-      options: {
-        headings: {
-                blogUrl: "blogUrl",
-                blogList: "blogList",
-                updateTime: "updateTime"
-            }
- 
-      }
-    };
-  },
-  created(){
-    axios.get("https://raw.githubusercontent.com/hexschool/w3hexschool-API/master/data.json")
-      .then(res => {
-        this.tableData = res.data
-    })
-  },
-}
-</script>
+    </div>
+  </template>
+
+  <script>
+  import axios from 'axios';
+  export default {
+    name: 'App',
+    data() {
+      return {
+        columns: [ "名字", "部落格文章","更新時間"],
+        tableData: [],
+        options: {
+          headings: {
+                  blogUrl: "名字",
+                  blogList: "部落格文章",
+                  updateTime: "更新時間"
+              }
+  
+        }
+      };
+    },
+    created(){
+      axios.get("https://raw.githubusercontent.com/hexschool/w3hexschool-API/master/data.json")
+        .then(res => {
+          this.tableData = res.data
+      })
+    },
+  }
+  </script>
 
 <style lang="scss">
 #app {
@@ -55,11 +60,6 @@ export default {
 table {
   background-color: transparent !important;
   table-layout: fixed !important;
-}
-.textover{
-  overflow:hidden;
-  white-space:nowrap;
-  text-overflow:ellipsis;
 }
 .col-md-12{
   display: flex;
--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//全局引入套件
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router' //全局引入router
+import {ClientTable, Event} from 'vue-tables-2'; //全局引入vue-tables-2套件
+import BootstrapVue  from 'bootstrap-vue'  //全局引入bootstrap-vue套件
+import axios from 'axios'; //全局引入axios套件
 import VueModalTor from "vue-modaltor";
-import {ServerTable, ClientTable, Event} from 'vue-tables-2';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+//全局註冊該套件
+Vue.use(ClientTable);  //註冊ClientTable
+Vue.use(BootstrapVue) //註冊BootstrapVue
+Vue.prototype.$http = axios //註冊axios
 Vue.use(VueModalTor);
-Vue.use(ClientTable, {}, false, 'bootstrap3', 'default'); //Client table
-Vue.use(ServerTable); //Server table
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+//全局引入bootstrap.css、bootstrap-vue.css
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-/* eslint-disable no-new */
+
+//el: #app意思就是會inject到html裡的<div id="app">
 new Vue({
   el: '#app',
   router,
