3a8f3edf834130e59e2e08d591fc4fe344dc34c2
comeuser85924 <zx2515296964@gmail.com>
2020-07-11 04:36:59 +0800

    update2.0

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    boxShadow: {
+      lg:'rgba(0, 0, 0, 0.2) 0px 0px 10px',
+    },
+    height: {
+      pc:'500px',
+      mb:'250px',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
